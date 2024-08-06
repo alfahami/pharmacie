7791fddff2d5112bba7d69a95e1f5f9f97b0a919
AL-FAHAMI <altoihir@gmail.com>
2024-08-06 19:27:02 +0100

    refractor: initialize spring

---
+++ src/main/java/com/pharma/pharmacy/PharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.pharma.pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyApplication.class, args);
+	}
+
+}
