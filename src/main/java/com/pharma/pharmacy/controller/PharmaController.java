fa42cd07af7b5de13638af1806c00f3a43a155d7
AL-FAHAMI <altoihir@gmail.com>
2024-08-06 20:04:00 +0100

    refractor: test routes

--- src/main/java/com/pharma/pharmacy/controller/PharmaController.java
+++ src/main/java/com/pharma/pharmacy/controller/PharmaController.java
@@ -1,5 +1,13 @@
 package com.pharma.pharmacy.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PharmaController {
     
+    @GetMapping("/")
+    public String getForm() {
+        return "home";
+    }
 }
