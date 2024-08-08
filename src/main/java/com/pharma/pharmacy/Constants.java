77db1f46c956d0eeeee52cec54a1b2c2a19e5a08
AL-FAHAMI <altoihir@gmail.com>
2024-08-08 12:00:30 +0100

    style: change links

--- src/main/java/com/pharma/pharmacy/Constants.java
+++ src/main/java/com/pharma/pharmacy/Constants.java
@@ -2,5 +2,5 @@ package com.pharma.pharmacy;
 
 public class Constants {
     
-    public final static String[] SUPPLIERS = {"B. Braun Melsungen AG", "Johnson & Johnson", "Novartis AG", "Abbott Laboratories"};
+    public static final int NOT_FOUND = 1000;
 }
