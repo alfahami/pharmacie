f8fd6f9332c78b178b332a076cb082164c5e53b4
AL-FAHAMI <altoihir@gmail.com>
2024-08-08 09:37:21 +0100

    feat: error handling

--- src/main/java/com/pharma/pharmacy/controller/PharmaController.java
+++ src/main/java/com/pharma/pharmacy/controller/PharmaController.java
@@ -1,13 +1,46 @@
 package com.pharma.pharmacy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.pharma.pharmacy.pojo.Drug;
+import com.pharma.pharmacy.service.PharmaService;
+
+
+import jakarta.validation.Valid;
 
 @Controller
 public class PharmaController {
     
+    @Autowired
+    PharmaService pharmaService;
+
     @GetMapping("/")
-    public String getForm() {
+    public String getHome() {
+        return "redirect:/home";
+    }
+
+    @GetMapping("/addDrug")
+    public String getForm(Model model) {
+        model.addAttribute("drug", new Drug());
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(@Valid Drug drug, BindingResult bindingResult) {
+
+         if(bindingResult.hasErrors()) return "form";
+        return "redirect:/home";
+
+    }
+
+    @GetMapping("/home")
+    public String getDrugs(Model model) {
+        model.addAttribute("drugs", pharmaService.getDrugs());
         return "home";
     }
 }
